16816b208e4f567e4bd6df14f8739602f143558c
Daan van der poel <200554@student.glu.nl>
2021-11-17 11:38:37 +0100

    AudioManager V1
    created the first version of the audio manager. i wil improve it later.

---
+++ Endless runner/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static public AudioManager instance;
+    public AudioClip[] audioClips;
+    public AudioSource[] audioSources;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
