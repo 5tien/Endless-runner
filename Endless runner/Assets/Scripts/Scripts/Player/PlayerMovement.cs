d6583cb7cebb8f2b5ac25c712792db22446b64e3
5tien <Sten.bosma00@gmail.com>
2021-11-17 11:18:31 +0100

    G

---
+++ Endless runner/Assets/Scripts/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jump_power;
+
+
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = this.transform.GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector3(0, 50));
+        }
+    }
+}
